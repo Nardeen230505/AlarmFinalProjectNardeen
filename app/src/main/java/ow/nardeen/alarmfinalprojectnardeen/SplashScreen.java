aac86c28e72bc3b7fd6d51e09b0cf71df3171e1d
Owner <lama76618@gmail.com>
2022-09-25 14:14:23 +0300

    Creat SplashScreen for real project

---
+++ app/src/main/java/ow/nardeen/alarmfinalprojectnardeen/SplashScreen.java
@@ -0,0 +1,27 @@
+package ow.nardeen.alarmfinalprojectnardeen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen); //يبني واجهة المستعمل بحيث تبني كل الكائنات الموجودة بملف التنسيق xml
+
+        Handler h=new Handler();
+        Runnable r=new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashScreen.this,SignInActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        h.postDelayed(r,2000);
+    }
+}
