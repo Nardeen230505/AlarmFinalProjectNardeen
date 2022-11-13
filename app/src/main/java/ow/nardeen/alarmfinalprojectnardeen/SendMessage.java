69b0cdc0d001576e5cdfc5d9af61d02e94ceabe5
Owner <lama76618@gmail.com>
2022-11-13 09:41:28 +0200

    creating send message actiivty

--- app/src/main/java/ow/nardeen/alarmfinalprojectnardeen/SendMessage.java
+++ app/src/main/java/ow/nardeen/alarmfinalprojectnardeen/SendMessage.java
@@ -1,14 +1,19 @@
 package ow.nardeen.alarmfinalprojectnardeen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SendMessage extends AppCompatActivity {
 
@@ -35,8 +40,13 @@ public class SendMessage extends AppCompatActivity {
                 // creat method
 
                    sendMessage();
+                }
 
-
+                else
+                {
+                    // when permission is not granted
+                    //request permission
+                    ActivityCompat.requestPermissions(SendMessage.this, new String[]{Manifest.permission.SEND_SMS}, 100);
                 }
             }
         });
@@ -45,6 +55,46 @@ public class SendMessage extends AppCompatActivity {
 
     private void sendMessage() {
         //get values from edit text
-        String sPhone=etPhone.getText().toString().trim();T
+        String sPhone=etPhone.getText().toString().trim();
+        String sMessage = etMessage.getText().toString().trim();
+        // check condition
+        if (!sPhone.equals("") && !sMessage.equals(""))
+        {
+            //when both edit text value not equal to blank
+            //initialize sms message
+            SmsManager smsManager=SmsManager.getDefault();
+            //send text message
+            smsManager.sendTextMessage(sPhone,null,sMessage,null,null);
+            //display toast
+            Toast.makeText(getApplicationContext(),"SMS sent successfuly!",Toast.LENGTH_LONG).show();
+        }
+
+        else {
+            // when edit text value is blank
+            // display toast
+            Toast.makeText(getApplicationContext(),"Enter value first.", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //check condition
+        if (requestCode==100 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            // when permission is granted
+            // call method
+            sendMessage();
+        }
+
+        else {
+            // when permission is denied
+            // display toast
+            Toast.makeText(getApplicationContext(), "Permission Denied!", Toast.LENGTH_SHORT).show();
+
+        }
+
     }
 }
