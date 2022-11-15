1608b8c568948c44189eb5cf9be129abed59fdd7
Owner <lama76618@gmail.com>
2022-11-15 14:12:35 +0200

    finishing the date and time dialog

--- app/src/main/java/ow/nardeen/alarmfinalprojectnardeen/AddTaskActivity1.java
+++ app/src/main/java/ow/nardeen/alarmfinalprojectnardeen/AddTaskActivity1.java
@@ -6,24 +6,34 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
 
 public class AddTaskActivity1 extends AppCompatActivity {
 
     private EditText etPhone,etMessage;
     private TextInputEditText etTask;
-    private EditText etTime;
-    private EditText etDate;
+    private EditText Date;
     private Button btnSaveAndSend;
+    private TextView mTimeTextView;
+    private EditText mPickTimeButton;
+    Context mcontext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +43,29 @@ public class AddTaskActivity1 extends AppCompatActivity {
 
         etPhone=findViewById(R.id.etPhone);
         etTask=findViewById(R.id.etMessage);
-        etTime=findViewById(R.id.etTime);
-        etDate=findViewById(R.id.etDate);
+        Date=findViewById(R.id.btnDate);
         btnSaveAndSend=findViewById(R.id.btnSaveAndSend);
         etMessage=findViewById(R.id.etMessage);
+        mTimeTextView = (TextView) findViewById(R.id.etDate);
+
+        Calendar calendar = Calendar.getInstance();
+        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        final int minute = calendar.get(Calendar.MINUTE);
+
+        mPickTimeButton = findViewById(R.id.pickTimebtn);
+
+        mPickTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(mcontext, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        mTimeTextView.setText(hourOfDay + ":" + minute);
+                    }
+                },hour, minute, android.text.format.DateFormat.is24HourFormat(mcontext));
+                timePickerDialog.show();
+            }
+        });
 
         btnSaveAndSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,7 +91,34 @@ public class AddTaskActivity1 extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.btnDate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                showDatePickerDialog();
+            }
+        });
+
     }
+
+    private void showDatePickerDialog()
+    {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int day, int month, int year) {
+                        Date.setText(day + "/" + month + "/" + year);
+
+                    }
+                },
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+
+    }
+
     private void sendMessage() {
         //get values from edit text
         String sPhone=etPhone.getText().toString().trim();
@@ -111,4 +167,10 @@ public class AddTaskActivity1 extends AppCompatActivity {
 
     }
 
+    /*@Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2)
+    {
+        String date = "month/day/year: " + datePicker.getMonth() + "/" + datePicker.getDayOfMonth() + "/" + datePicker.getYear();
+
+    }*/
 }
