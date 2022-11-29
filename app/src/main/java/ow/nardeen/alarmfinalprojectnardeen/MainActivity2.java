40b9265bb7391a40def3e8770e1996518eb3ead0
Owner <lama76618@gmail.com>
2022-11-29 10:51:34 +0200

    working on the sms broadcast

--- app/src/main/java/ow/nardeen/alarmfinalprojectnardeen/MainActivity2.java
+++ app/src/main/java/ow/nardeen/alarmfinalprojectnardeen/MainActivity2.java
@@ -183,6 +183,19 @@ public class MainActivity2 extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Android apps can send or receive broadcast messages from the Android system and other Android apps,
+     * similar to the publish-subscribe design pattern. These broadcasts are sent when an event of interest occurs.
+     * For example, the Android system sends broadcasts when various system events occur,
+     * such as when the system boots up or the device starts charging. Apps can also send custom broadcasts,
+     * for example, to notify other apps of something that they might be interested in (for example, some new data has been downloaded).
+     *
+     * Apps can register to receive specific broadcasts.
+     * When a broadcast is sent,
+     * the system automatically routes broadcasts to apps that have subscribed to receive that particular type of broadcast.
+     * @param view
+     */
+
     public void btnSendBroadcast(View view)
     {
         Intent intent = new Intent();
@@ -197,8 +210,8 @@ public class MainActivity2 extends AppCompatActivity {
                     PackageManager.PERMISSION_GRANTED  )&& (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) !=
                     PackageManager.PERMISSION_GRANTED  ))
             {
-                requestPermissions(new String[]{
-                                android.Manifest.permission.RECEIVE_SMS},
+                requestPermissions(new String[] {
+                        android.Manifest.permission.RECEIVE_SMS  },
                         REQUEST_CODE_ASK_PERMISSIONS);
                 return ;
             }
